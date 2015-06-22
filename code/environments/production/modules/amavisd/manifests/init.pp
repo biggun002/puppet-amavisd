9fdc6a8ce3ceb75b6f7ef97e584ab2d8b0c802dd
Charlie Root <root@example.com>
2015-06-22 15:59:50 +0700

    fix amavisd module

--- code/environments/production/modules/amavisd/manifests/init.pp
+++ code/environments/production/modules/amavisd/manifests/init.pp
@@ -1,10 +1,10 @@
 class amavisd{
-	package{'amavisd':
+	package{'amavisd-new':
 		ensure => 'present',
 	}
 	file{'/usr/local/etc/amavisd.conf':
 		ensure => 'file',
 		source => 'puppet:///modules/amavisd/amavisd.conf',
-		require => 'Package['amavisd'],
+		require => Package['amavisd'],
 	}
 }
