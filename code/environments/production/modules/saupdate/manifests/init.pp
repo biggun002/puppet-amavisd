630660c9e37253eb80d2f252ede1bbf3f939fd8d
puppetmaster <root@puppetmaster>
2015-07-11 11:03:22 +0700

    full path

--- code/environments/production/modules/saupdate/manifests/init.pp
+++ code/environments/production/modules/saupdate/manifests/init.pp
@@ -13,14 +13,14 @@ class saupdate{
 		path => '/usr/local/bin',
 		cwd => '/var/db/spamassassin/3.004001',
 		command => 'wget apache.claz.org//spamassassin/source/Mail-SpamAssassin-rules-3.4.1.r1675274.tgz',
-		creates => 'Mail-SpamAssassin-rules-3.4.1.r1675274.tgz',
+		creates => '/var/db/spamassassin/3.004001/Mail-SpamAssassin-rules-3.4.1.r1675274.tgz',
 		require => [File['/var/db/spamassassin/3.004001'],Package['wget']],
 	}
 	exec{'tarupdate':
 		path => '/usr/bin',
 		cwd => '/var/db/spamassassin/3.004001',
 		command => 'tar -xvf Mail-SpamAssassin-rules-3.4.1.r1675274.tgz',
-		creates => '60_awl.cf',
+		creates => '/var/db/spamassassin/3.004001/60_awl.cf',
 		require => Exec['wgetupdate'],
 	}
 }
